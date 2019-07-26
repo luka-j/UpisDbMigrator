6884da446ef6f9f3c418b4a300fed4b9bd8b94e5
Luka Jovicic <luka.jovicic16@gmail.com>
2019-07-26 18:17:39 +0200

    2019 evolution
    Signed-off-by: luka <luka.jovicic16@gmail.com>

--- src/main/java/rs/lukaj/upisstats/migrator/Main.java
+++ src/main/java/rs/lukaj/upisstats/migrator/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static final boolean LOG = true;
     public static final boolean OVERWRITE_DATA = true;
-    public static final int     DB_YEAR        = 2018;
-    public static final String  DATA_YEAR      = "18";
+    public static final int     DB_YEAR        = 2019;
+    public static final String  DATA_YEAR      = "19";
 
     public static void main(String[] args) {
         String user = System.getenv("PG_USER");
